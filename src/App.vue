c3ead4abc90281b8dd0a2cb9ed462da9b1190102
Matevz Stinjek <matevz.stinjek@celtra.com>
2019-04-16 10:16:51 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <md-dialog :md-active.sync="showDialog">
+            <md-dialog-title>Preferences</md-dialog-title>
+
+            <md-field>
+                <label>Name</label>
+                <md-input v-model="name"></md-input>
+            </md-field>
+
+            <md-field>
+                <label>Amount</label>
+                <span class="md-prefix">$</span>
+                <md-input class="md-layout-item" v-model="amount"></md-input>
+            </md-field>
+
+            <md-button class="md-raised md-primary" @click="saveLog">Save</md-button>
+        </md-dialog>
+
+        <md-button class="md-fab md-primary" @click="showDialog = true">
+            <md-icon>add</md-icon>
+        </md-button>
+
+        <div v-for="log in logs" :key="`${log}-${Math.random()}`">{{ `${log.name}: ${log.amount}` }}</div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+const logs = this.$gun.get('logs')
+const username = 'matevz'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    data () {
+        return {
+            showDialog: false,
+            name: null,
+            amount: null,
+            logs: []
+        }
+    },
+    methods: {
+        saveLog () {
+            const newLog = logs.get(Date.now().toString())
+            newLog.put({
+                name: this.name,
+                amount: this.amount
+            })
+            logs.set(newLog);
+            this.name = null
+            this.amount = null
+            this.showDialog = false
+        },
+        logsUpdated (log) {
+            this.logs.push(log)
+        }
+    },
+    created () {
+        logs.map().on(this.logsUpdated.bind(this));
+    }
 }
 </script>
 
 <style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default-dark.css'
+import Gun from 'gun'
 
+Vue.use(VueMaterial)
+Vue.prototype.$gun = new Gun()
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
